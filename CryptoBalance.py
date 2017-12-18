de29b9e57fa2cf66bcf52eb75649b6bf64bea1e3
feetball <daniel@kuecker.net>
2017-12-18 13:16:00 -0600

    adding requests_toolbox back for google

--- CryptoBalance.py
+++ CryptoBalance.py
@@ -3,11 +3,11 @@ import requests, pdb, ast, os
 import pprint
 import pdb
 
-#import requests_toolbelt.adapters.appengine
+import requests_toolbelt.adapters.appengine
 
 # Use the App Engine Requests adapter. This makes sure that Requests uses
 # URLFetch.
-#requests_toolbelt.adapters.appengine.monkeypatch()
+requests_toolbelt.adapters.appengine.monkeypatch()
 
 app = Flask(__name__)
 
