a4fb2a10d8ac6fb6f6d1a546f36901826a47eec6
Daniel Kuecker <daniel@kuecker.net>
2017-12-18 13:47:57 -0600

    Moving keys to google db

--- CryptoBalance.py
+++ CryptoBalance.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, json, request
+from google.appengine.ext import ndb
 import requests, pdb, ast, os
 import pprint
 import pdb
@@ -64,16 +65,15 @@ def GetCoinBalance(coinName, walletAddr):
 
     if coinName == 'xrp' :
         url = 'https://data.ripple.com/v2/accounts/' + walletAddr
-        #pdb.set_trace()
         coin_balance_request = ast.literal_eval(requests.get(url).text)
         if coin_balance_request['result'] in 'success':
             coin_balance = float(coin_balance_request['account_data']['initial_balance'])
     elif coinName == 'eth':
-        key =  os.environ['etherscan_key']
+        key =  Settings.get('etherscan_key')
         url = 'https://api.etherscan.io/api?module=account&action=balance&tag=latest&apikey=' + key + '&address=' + walletAddr
         coin_balance = float(ast.literal_eval(requests.get(url).text)['result'])/10e18
     else:
-        key = 'key=' + os.environ['cryptoid_key']
+        key = 'key=' + Settings.get('cryptoid_key')
         url = 'https://chainz.cryptoid.info/' + coinName + '/api.dws?q=getbalance&' + key + '&a=' + walletAddr
         coin_balance = float(requests.get(url).text)
 
@@ -93,6 +93,25 @@ def GetCoinPricesUSD():
 
     return coin_prices_dict
 
+
+class Settings(ndb.Model):
+    name = ndb.StringProperty()
+    value = ndb.StringProperty()
+
+    @staticmethod
+    def get(name):
+        NOT_SET_VALUE = "NOT SET"
+        retval = Settings.query(Settings.name == name).get()
+        if not retval:
+            retval = Settings()
+            retval.name = name
+            retval.value = NOT_SET_VALUE
+            retval.put()
+        if retval.value == NOT_SET_VALUE:
+            raise Exception(('Setting %s not found in the database. A placeholder record has been created.' +
+               ' Go to the Developers Console for your app in App Engine, look up the Settings record with ' + 
+               'name=%s and enter its value in that records value field.') % (name, name))
+        return retval.value
 
 if __name__ == "__main__":
 	app.run()
