6d628ee7976de4881dac91768f59da8d29271072
feetball <daniel@kuecker.net>
2017-12-18 10:39:55 -0600

    Adding functionality.  Adding app.yaml for google cloud.

--- CryptoBalance.py
+++ CryptoBalance.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, json, request
 import requests, pdb, ast, os
 import pprint
+
+import requests_toolbelt.adapters.appengine
+
+# Use the App Engine Requests adapter. This makes sure that Requests uses
+# URLFetch.
+requests_toolbelt.adapters.appengine.monkeypatch()
+
 app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'Please send coin and wallet addresses via URL.  Example: https://.../balance?ltc=<ltc_wallet_address>&btc=<btc_wallet_address>&xrp=<xrp_wallet_address> \n  Only ltc, btc and xrp are supported at this time.'
 
 @app.after_request
 def treat_as_plain_text(response):
@@ -13,6 +25,8 @@ def treat_as_plain_text(response):
 def show_balance():
     price_dict = {}
     balances = ""
+    coin_price_string = '\nCoin Prices: \n'
+    total_balance = 0
 
     #print 'cryptoid key: ' + os.environ['cryptoid_key']
 
@@ -30,6 +44,11 @@ def show_balance():
     if price_dict:
         for key, value in price_dict.items():
             balances = balances + key + ' balance (USD): $' +  value + '\n'
+            total_balance = total_balance + float(value)
+            coin_price_string = coin_price_string + key + '(USD) $: ' + filter(lambda coin_price: coin_price['symbol'] == key.upper(), coin_prices)[0]['price_usd'] + '\n'
+
+        balances = balances + 'Total Balance (USD): $' + str(total_balance) + '\n' + coin_price_string         
+
     else:
         return 'No valid wallet addresses privided'
 
